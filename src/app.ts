b3a0ef1cf70b0dba0c0e1aeb1db2a66048ef3969
ArnoGhazaryan <ghazaryanarno@gmail.com>
2023-12-14 23:10:02 +0400

    Revert "deletion of redundant codes and change README"
    This reverts commit 8db2506be840b13b03d07c80a16b8b50ef6f2688.

--- src/app.ts
+++ src/app.ts
@@ -7,25 +7,33 @@ class Ball {
   x: number;
   y: number;
   dy: number;
+  dx: number;
   size: number;
   color: string;
   gravity: number;
+  friction: number;
   bounceFactor: number;
+  isHorizontal: boolean;
 
   constructor(
     x: number,
     y: number,
     size: number,
     gravity: number,
-    color: string
+    color: string,
+    isHorizontal: boolean
   ) {
     this.x = x;
     this.y = y;
     this.dy = 0; // Vertical speed
+    this.dx = Math.random() * 10; // Increased horizontal speed
     this.size = size;
     this.color = color;
     this.gravity = gravity; // Gravity by difault Earth
+    this.friction = gravity.toFixed().toString().length === 1 ? 0.9995 :
+                    gravity.toFixed().toString().length === 2 ? 0.995 : 0.89 // Horizontal friction
     this.bounceFactor = 0.8; // Dampening effect on each bounce
+    this.isHorizontal = isHorizontal;
   }
 
   draw() {
@@ -37,6 +45,8 @@ class Ball {
   }
 
   update(deltaTime: number) {
+    console.log(this.gravity.toFixed().toString().length);
+    
     // Apply speed
     this.dy += this.gravity * deltaTime;
 
@@ -54,16 +64,37 @@ class Ball {
       this.dy = 0;
     }
 
-    if (this.x - this.size < 0) {
-      this.x = this.size;
-    } else if (this.x + this.size > canvas.width) {
-      this.x = canvas.width - this.size;
+    if (this.isHorizontal) {
+      // Update position horizontal
+      this.x += this.dx;
+
+      // Apply friction
+      this.dx *= this.friction;
+
+      // Bounce are reflected after hitting the sides
+      if (this.x - this.size < 0) {
+        this.dx = Math.abs(this.dx);
+      } else if (this.x + this.size > canvas.width) {
+        this.dx = -Math.abs(this.dx) * this.bounceFactor;
+      }
+
+      if (Math.abs(this.dx) < 0.1) {
+        this.dx = 0;
+      }
+      console.log(this.dx);
+    } else {
+      if (this.x - this.size < 0) {
+        this.x = this.size;
+      } else if (this.x + this.size > canvas.width) {
+        this.x = canvas.width - this.size;
+      }
     }
   }
 }
 
 const balls: Ball[] = [];
 let gravity: number = 9.81;
+let isHorizontal: boolean = false;
 
 // Event listener for canvas click to create new balls
 canvas.addEventListener("click", (e) => {
@@ -80,18 +111,16 @@ canvas.addEventListener("click", (e) => {
   }
 
   // Create a new ball and add it to the array
-  const ball = new Ball(x, y, size, gravity, color.value);
+  const ball = new Ball(x, y, size, gravity, color.value, isHorizontal);
   if (balls.length === 15) {
     balls.shift(); // Remove the oldest ball if the limit is reached
   }
   balls.push(ball);
 });
 
-let lastTime: number = performance.now(); //Resolution timestamp in milliseconds
+let lastTime: number = performance.now();
 
 function tick(currentTime: number) {
-  console.log(lastTime);
-  
   const deltaTime: number = (currentTime - lastTime) / 1000; // Convert to seconds
   context.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -109,7 +138,8 @@ const planetBtns: NodeListOf<Element> =
   document.querySelectorAll(".gravityPlanets");
 const minusBtn: Element = document.querySelector("#minusBtn")!;
 const plusBtn: Element = document.querySelector("#plusBtn")!;
-const sizeBtn: Element = document.querySelector("#ballSize")!;
+const radiusBtn: Element = document.querySelector("#ballSize")!;
+// const horizontalBtn: Element = document.querySelector("#horizontalBtn")!;
 
 // Function for pressing planet buttons and selecting gravity, default 9.81
 planetBtns.forEach((planetBtn) => {
@@ -121,31 +151,36 @@ planetBtns.forEach((planetBtn) => {
 
 // Function for pressing the radius button minus a number
 minusBtn.addEventListener("click", () => {
-  const ballSize: number = Number(
+  const radiusNum: number = Number(
     document.querySelector("#ballSize")!.innerHTML
   );
 
   // Decrease ball size with a lower limit of 0
-  if (ballSize === 0) {
-    sizeBtn.textContent = String(0);
+  if (radiusNum === 0) {
+    radiusBtn.textContent = String(0);
   } else {
-    sizeBtn.textContent = String(ballSize - 1);
+    radiusBtn.textContent = String(radiusNum - 1);
   }
 });
 
 // Function for pressing the radius button plus a number
 plusBtn.addEventListener("click", () => {
-  const ballSize: number = Number(
+  const radiusNum: number = Number(
     document.querySelector("#ballSize")!.innerHTML
   );
 
   // Increase ball size with an upper limit of 50
-  if (ballSize === 50) {
-    sizeBtn.textContent = String(ballSize);
+  if (radiusNum === 50) {
+    radiusBtn.textContent = String(radiusNum);
   } else {
-    sizeBtn.textContent = String(ballSize + 1);
+    radiusBtn.textContent = String(radiusNum + 1);
   }
 });
+
+// Function for pressing the button on or off horizontal bouncing
+// horizontalBtn.addEventListener("click", () => {
+//   isHorizontal = !isHorizontal;
+// });
 
 // The text is divided by letter to rotate the text in a circle
 const texts: NodeListOf<Element> = document.querySelectorAll("h3")!;
@@ -153,7 +188,7 @@ texts.forEach((text: Element) => {
   text.innerHTML = text.innerHTML
     .split("")
     .map(
-      (char, i) => `<span style="transform:rotate(${i * 45}deg)">${char}</span>`
+      (char, i) => `<span style="transform:rotate(${i * 50}deg)">${char}</span>`
     )
     .join("");
 });
